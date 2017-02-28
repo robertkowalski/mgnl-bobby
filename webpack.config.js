59b95fbfe3d3474cf9942de6ed0d69e20c6ab0a9
Robert Kowalski <rok@kowalski.gd>
2017-02-28 11:15:15 +0100

    several fixes:
     - adjust npmignore
     - change dist location
     - use _dev folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src-webresources/app.js',
+  entry: './_dev/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'webresources', 'js')
+    path: path.resolve(__dirname, 'webresources')
   },
 
   module: {
